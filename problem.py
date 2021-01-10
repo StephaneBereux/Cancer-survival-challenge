ee7fb3f995d04550d2bf8a58f2dac30a7b6beaf6
Stéphane <stephane@salve-regina.home>
2021-01-10 23:12:50 +0100

    preparing problem.py

--- problem.py
+++ problem.py
@@ -2,6 +2,10 @@
 import numpy as np
 import pandas as pd
 import time as time
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 
 # for gene names converting
 import mygene as mg
@@ -9,6 +13,7 @@ import mygene as mg
 # for data fetching
 import xenaPython as xena
 
+data_dir = 'data'
 
 def get_codes(host, dataset, fields, data):
     """Get codes for enumerations."""
@@ -102,7 +107,22 @@ def get_survival_data(hub, dataset, samples):
     fields = ['OS', "OS.time", "sampleID"]
     values = get_fields_and_codes(hub, dataset, samples, fields)
     survival_dict = dict(zip(fields, values))  # index by phenotype
-    survival_dict["survival"] = survival_dict.pop("OS.time")
+    survival_dict["time"] = survival_dict.pop("OS.time")
+    survival_dict["death"] = survival_dict.pop("OS")
+    df_survival = pd.DataFrame(data=survival_dict)
+    return df_survival
+
+
+def get_all_survival_data(samples):
+    hub = 'https://tcga.xenahubs.net'
+    dataset = "survival/BRCA_survival.txt"
+    samples = xena.dataset_samples(hub, dataset, None)
+    # OS is 1 if the patient has died or 0 if he was censored (lost...)
+    fields = ['DFI', 'DFI.time', 'DSS', 'DSS.time', 'OS', 'OS.time', 'PFI', 'PFI.time', 'sampleID']
+    #fields = ['OS', "OS.time", "sampleID"]
+    values = get_fields_and_codes(hub, dataset, samples, fields)
+    survival_dict = dict(zip(fields, values))  # index by phenotype
+    survival_dict["time"] = survival_dict.pop("OS.time")
     survival_dict["death"] = survival_dict.pop("OS")
     df_survival = pd.DataFrame(data=survival_dict)
     return df_survival
@@ -147,7 +167,7 @@ def merge_data(df_expression, df_survival, df_phenotype=None):
     """Merge the expression data with the survival data."""
     df = pd.merge(df_expression, df_survival)  # Merge according to sampleID
     df = df.apply(pd.to_numeric, errors='coerce')
-    df = df[~pd.isna(df["survival"])][~pd.isna(df["death"])]
+    df = df[~pd.isna(df["time"])][~pd.isna(df["death"])]
 
     # To remove duplicates and/or add supplementary phenotypical covariates
     if df_phenotype is not None:
@@ -166,25 +186,113 @@ def download_data(database_filename):
 
     # Survival data
     df_survival = get_survival_data(hub, dataset, samples)
-
+    
     # Phenotype data
     # df_phenotype = get_phenotype_data(hub, samples)
 
     df_all = merge_data(df_expression, df_survival)
-    df_all.to_csv(database_filename, index=False)
-
-    return df_all
-
+    
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    database_path = os.path.join(data_dir, database_filename)
+    filtered_df = filter_outliers(df_all)
+    filtered_df.to_csv(database_path, index=False)
+    return filtered_df
+    
 
 def load_or_download(database_filename):
     """Return the database (either by loading it, or by download it)."""
+    data_dir = 'data'
+    database_path = os.path.join(data_dir, database_filename)
     try:
         df = pd.read_csv(database_filename)
     except FileNotFoundError:
         df = download_data(database_filename)
     return df
 
+def split_train_test(df):
+    
+    index_train, index_test = train_test_split( range(len(df)), test_size = 0.2, random_state=10)
+    data_train = df.loc[index_train].reset_index( drop = True )
+    data_test  = df.loc[index_test].reset_index( drop = True )
 
-if __name__ == '__main__':
-    database_filename = 'database_TCGA.csv'
-    load_or_download(database_filename)
+
+def filter_outliers(df):
+    """The events occuring after 4500 are too rare and must be filtered, otherwise the Integrated Brier Score makes no sense."""
+    long_times = df[df['time'] > 4500].index.to_numpy()
+    return df.drop(long_times).reset_index()
+
+
+def get_train_data():
+    """Return the training data."""
+    return
+
+def get_test_data():
+    """Return the test data."""
+    return
+
+
+problem_title = 'Breast cancer survival prediction'
+
+score_types = [
+    ConcordanceIndex(name='concordance_index'),
+    IntegratedBrierScore(name='integrated_brier_score')
+]
+
+
+def get_cv(X, y):
+    test = os.getenv('RAMP_TEST_MODE', 0)
+    n_splits = 8
+    if test:
+        n_splits = 2
+    spliter = GroupShuffleSplit(n_splits=n_splits, test_size=.2,
+                            random_state=42)
+    non_censored = X['death']
+    splits = spliter.split(X, y, non_censored)
+
+    # take only 500 samples per test subject for speed
+    def limit_test_size(splits):
+        rng = np.random.RandomState(42)
+        for train, test in splits:
+            yield (train, test[rng.permutation(len(test))[:500]])
+    return limit_test_size(splits)
+
+
+def _read_data(path, dir_name):
+    DATA_HOME = path
+    X_df = pd.read_csv(os.path.join(DATA_HOME,
+                                    DATA_PATH,
+                                    dir_name, 'X.csv.gz'))
+    X_df.iloc[:, :-1] *= 1e12  # scale data to avoid tiny numbers
+
+    # add a new column lead_field where you will insert a path to the
+    # lead_field for each subject
+    lead_field_files = os.path.join(DATA_HOME, DATA_PATH, '*L.npz')
+    lead_field_files = sorted(glob.glob(lead_field_files))
+
+    lead_subject = {}
+    # add a row with the path to the correct LeadField to each subject
+    for key in np.unique(X_df['subject']):
+        path_L = [s for s in lead_field_files if key + '_L' in s][0]
+        lead_subject[key] = path_L
+    X_df['L_path'] = X_df.apply(lambda row: lead_subject[row.subject], axis=1)
+
+    y = sparse.load_npz(
+        os.path.join(DATA_HOME, DATA_PATH, dir_name, 'target.npz')).toarray()
+
+        return X_df, y
+
+
+def get_train_data(path="."):
+    return _read_data(path, 'train')
+
+
+def get_test_data(path="."):
+    return _read_data(path, 'test')
+
+
+
+#if __name__ == '__main__':
+#    database_filename = 'test_database_TCGA.csv'
+#   df = load_or_download(database_filename)
+    
