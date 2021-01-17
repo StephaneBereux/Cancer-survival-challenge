8308b6c2c5ce5cd993cca51b9f9020ebec8c2674
Stéphane <stephane@salve-regina.home>
2021-01-17 10:14:09 +0100

    added todolist

--- submissions/starting_kit/regressor.py
+++ submissions/starting_kit/regressor.py
@@ -1,21 +1,36 @@
+from sksurv.linear_model import CoxPHSurvivalAnalysis
+from problem import to_structured_array
+
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.decomposition import PCA
-from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
+from sklearn.compose import make_column_transformer
+from sklearn.linear_model import LinearRegression
 
 
+def get_unexpressed_columns(X_train):
+    mean_x = X_train.mean()
+    unexpressed_columns = X_train.columns[(mean_x == 0.).to_numpy()]
+    unexpressed_columns = [column.split('\n')[0] for column in unexpressed_columns]
+    return unexpressed_columns
+    
+    
 class Regressor(BaseEstimator, RegressorMixin):
     def __init__(self):
         pca = PCA(n_components=10)
-        regressor = LinearRegression(n_jobs=-1)
+        regressor = CoxPHSurvivalAnalysis()
         self.regr = Pipeline([('pca', pca), ('regressor', regressor)])
         return 
 
-    def fit(self, X, y=None):
-        y_to_predict = y[:,1] # We are only interested in predicting the survival time, not the censoring
-        self.regr.fit(X,y_to_predict)
+    def fit(self, X, E_y=None):
+        self.to_drop_columns = get_unexpressed_columns(X)
+        X.drop(columns=self.to_drop_columns, inplace=True)
+        struct_E_y = to_structured_array(E_y)
+        self.regr.fit(X, struct_E_y)
         return self
     
     def predict(self, X):
-        return self.regr.predict(X)
-
+        X.drop(columns=self.to_drop_columns, inplace=True)
+        risk_pred = self.regr.predict(X)
+        y_pred = (max(risk_pred) + 1) - risk_pred
+        return y_pred
